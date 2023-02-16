c22d4947e32b6d6980ac7b5aada132710fd8ba73
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-02-16 05:53:30 -0300

    [ADD] Creación de la arquitectura MVC y creacion de usuario

--- curso/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ curso/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -1,12 +1,48 @@
 package com.cursojava.curso.controllers;
 
+import com.cursojava.curso.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class UsuarioController {
-    @RequestMapping(value = "prueba")
-    public String prueba(){
-        return "Prueba";
+    @RequestMapping(value = "usuario/{id}")
+        public Usuario getUsuario(@PathVariable String id){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Braian");
+        usuario.setApellido("Troncoso");
+        usuario.setEmail("troncosobraianaxel@gmail.com");
+        usuario.setTelefono("019288382");
+        return usuario;
+    }
+    @RequestMapping(value = "usuario")
+    public Usuario modificar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Braian");
+        usuario.setApellido("Troncoso");
+        usuario.setEmail("troncosobraianaxel@gmail.com");
+        usuario.setTelefono("019288382");
+        return usuario;
+    }
+    @RequestMapping(value = "usuario")
+    public Usuario eliminar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Braian");
+        usuario.setApellido("Troncoso");
+        usuario.setEmail("troncosobraianaxel@gmail.com");
+        usuario.setTelefono("019288382");
+        return usuario;
+    }
+    @RequestMapping(value = "usuario")
+    public Usuario buscar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Braian");
+        usuario.setApellido("Troncoso");
+        usuario.setEmail("troncosobraianaxel@gmail.com");
+        usuario.setTelefono("019288382");
+        return usuario;
     }
 }
