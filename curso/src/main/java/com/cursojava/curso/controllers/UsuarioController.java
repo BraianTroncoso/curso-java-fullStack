c6afac04d9c70fa6e36b366a1e36f5ee469ebfc0
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-02-16 08:22:11 -0300

    [ADD] Llamar a una API con AJAX

--- curso/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ curso/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -5,20 +5,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UsuarioController {
     @RequestMapping(value = "usuario/{id}")
-        public Usuario getUsuario(@PathVariable String id){
+        public Usuario getUsuario(){
         Usuario usuario = new Usuario();
         usuario.setNombre("Braian");
         usuario.setApellido("Troncoso");
         usuario.setEmail("troncosobraianaxel@gmail.com");
         usuario.setTelefono("019288382");
+        usuario.setId(1L);
         return usuario;
     }
-    @RequestMapping(value = "usuario")
+    @RequestMapping(value = "usuario123")
     public Usuario modificar(){
         Usuario usuario = new Usuario();
         usuario.setNombre("Braian");
@@ -27,7 +29,7 @@ public class UsuarioController {
         usuario.setTelefono("019288382");
         return usuario;
     }
-    @RequestMapping(value = "usuario")
+    @RequestMapping(value = "usuario432")
     public Usuario eliminar(){
         Usuario usuario = new Usuario();
         usuario.setNombre("Braian");
@@ -36,7 +38,7 @@ public class UsuarioController {
         usuario.setTelefono("019288382");
         return usuario;
     }
-    @RequestMapping(value = "usuario")
+    @RequestMapping(value = "usuario654")
     public Usuario buscar(){
         Usuario usuario = new Usuario();
         usuario.setNombre("Braian");
@@ -44,5 +46,37 @@ public class UsuarioController {
         usuario.setEmail("troncosobraianaxel@gmail.com");
         usuario.setTelefono("019288382");
         return usuario;
+    }
+    @RequestMapping(value = "usuarios")
+    public List<Usuario> getUsuarios(@PathVariable Long id){ //tengo un error del metodo getUsuarios
+        List<Usuario> usuarios = new ArrayList<>();
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Braian");
+        usuario.setApellido("Troncoso");
+        usuario.setEmail("troncosobraianaxel@gmail.com");
+        usuario.setTelefono("019288382");
+        usuario.setId(3L);
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setNombre("Javier");
+        usuario2.setApellido("Troncoso");
+        usuario2.setEmail("javier@gmail.com");
+        usuario2.setTelefono("019288382");
+        usuario2.setId(4L);
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setNombre("Lautaro");
+        usuario3.setApellido("Troncoso");
+        usuario3.setEmail("Lautaro@gmail.com");
+        usuario3.setTelefono("019288382");
+        usuario3.setId(5L);
+
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+        return usuarios;
+
+
     }
 }
